b1f34dbc617350281ceb96f3343512cc40f9d469
davinkurniawan <davinkurniawan07@gmail.com>
2015-09-18 15:10:47 +1000

    sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,18 +1,18 @@
-drop table book;
+drop table publications;
 drop table users;
 drop table admin;
 
-create table book (
-	id int not null generated always as identity,
-	title varchar(20) not null,
-	price int not null,
+create table publications (
+	id integer not null generated always as identity,
+	title varchar(40) not null,
+	price integer not null,
 	constraint chk_price check
 	(price>=0),
 	author varchar(20) not null,
 	pub_type varchar(20) not null, 
 	constraint chk_type check
 	(pub_type='book/collection' or pub_type='journal' or pub_type='conference' or pub_type='editorship'),
-	pub_year int,
+	pub_year numeric(4,0),
 	isbn varchar(20),
 	picture varchar (100),
 	pause boolean not null,
@@ -20,19 +20,21 @@ create table book (
 );
 
 create table users (
-	username varchar(20) not null unique,
+	username varchar(20) not null,
 	password varchar(20) not null, 
-	email varchar(30) not null,
+	email varchar(30) not null unique,
 	nickname varchar(10) not null,
 	first_name varchar(20) not null,
 	last_name varchar(20) not null,
-	birth_year int(4) not null, 
+	birth_year numeric(4,0) not null, 
 	address varchar(100) not null,
-	credit_card varchar(20) not null,
+	credit_card bigint not null,
  	primary key (username)
 );
 
 create table admin (
-	foreign key (user_id) references users(username),
-	foreign key (book_id) references book(id),
+	admin_id integer not null generated always as identity,
+	book_title varchar(40) not null,
+	foreign key (admin_id) references users(username),
+	foreign key (book_title) references publications(id)
 );
