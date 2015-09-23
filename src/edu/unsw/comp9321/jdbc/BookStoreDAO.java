73957f973bdc7f2eac1256d1a92c877fc0c2fbb8
davinkurniawan <davinkurniawan07@gmail.com>
2015-09-23 17:21:55 +1000

    bookstoredao

--- src/edu/unsw/comp9321/jdbc/BookStoreDAO.java
+++ src/edu/unsw/comp9321/jdbc/BookStoreDAO.java
@@ -26,33 +26,19 @@ public class BookStoreDAO {
 	
 	public void addUser(UserDTO u) throws DataAccessException {
 		try {
-			con = services.createConnection();
-			PreparedStatement stmt = con
-					.prepareStatement("insert into tbl_contacts (owner_id, short_name, full_name, email, contact_number, notes) values (?, ?, ?, ?, ?, ?)");
-			stmt.setInt(1, bean.getOwner());
-			stmt.setString(2, bean.getShortName());
-			stmt.setString(3, bean.getFullName());
-			stmt.setString(4, bean.getEmail());
-			stmt.setString(5, bean.getContactNumber());
-			stmt.setString(6, bean.getNotes());
+			PreparedStatement stmt = connection
+					.prepareStatement("insert into users (username, password, email) values (?, ?, ?)");
+			stmt.setString(1, u.getUsername());
+			stmt.setString(2, u.getPassword());
+			stmt.setString(3, u.getEmail());
 			int n = stmt.executeUpdate();
-			if (n != 1)
+			if (n != 1) {
 				throw new DataAccessException(
 						"Did not insert one row into database");
-		} catch (ServiceLocatorException e) {
-			throw new DataAccessException("Unable to retrieve connection; "
-					+ e.getMessage(), e);
+			}
 		} catch (SQLException e) {
 			throw new DataAccessException("Unable to execute query; "
 					+ e.getMessage(), e);
-		} finally {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-			}
 		}
 	}
 }
