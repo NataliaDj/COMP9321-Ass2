3e65f0a73b2e03c2008beed4b065f5f690cc1fb0
Vincent Rubingh <vincentrubingh@gmail.com>
2015-09-26 17:10:25 +1000

    Revert "Created Utilities class and changed RegisterCommand"
    This reverts commit 12f082ee8af360c3e748759c7ced928870ede38d.

--- src/edu/unsw/comp9321/web/RegisterCommand.java
+++ src/edu/unsw/comp9321/web/RegisterCommand.java
@@ -30,37 +30,34 @@ public class RegisterCommand implements Command{
 		}
 		 
 		if (action.equals("registering")) { //success
-			register(request, response);
-			 
-		} else if (action.equals("activation")) {
 			response.setContentType("text/html");// from response, set content type
 			PrintWriter out = response.getWriter();// from response, get output writer
-			out.println("<b>Activating " + request.getParameter("username") + "!</b>"); 
-			
+			out.println("<b>Submitting!</b>"); 
+			 
+			UserDTO user = CreateUser(request, response);
 			UserService service = new UserService();
-			service.activateUser(request.getParameter("username")); // NEED ERROR HANDLING
+			service.addUser(user);
+			//TEMP DISABLED//UserDelegate user_del = DelegateFactory.getInstance().getUserDelegate();
+			//TEMP DISABLED//user_del.addUser(userbean);
+			 
+		} else if (action.equals("activating")) {
+			
 			
 		} else {
 			String nextPage = "register.jsp";
 			RequestDispatcher rd = request.getRequestDispatcher("/"+nextPage);
 			rd.forward(request, response); 
 		}
-
+		
+		
+		
+		
+		//response.setContentType("text/html");// from response, set content type
+		//PrintWriter out = response.getWriter();// from response, get output writer
+		//out.println("<b>REGISTERRR</b>");
 		return null;
 	}
 	
-	
-	private void register(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.setContentType("text/html");// from response, set content type
-		PrintWriter out = response.getWriter();// from response, get output writer
-		out.println("<b>Submitting!</b>"); 
-		 
-		UserDTO user = CreateUser(request, response);
-		UserService service = new UserService();
-		service.addUser(user);
-		//TEMP DISABLED//UserDelegate user_del = DelegateFactory.getInstance().getUserDelegate();
-		//TEMP DISABLED//user_del.addUser(userbean);
-	}
 	
 	private UserDTO CreateUser(HttpServletRequest request, HttpServletResponse response) 
 			throws IOException {
