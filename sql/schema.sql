d8b57f3fd6467826767daf95994e5cdbf72a9f4d
Tony Ward <tony.ward@gmail.com>
2015-09-29 06:56:01 +1000

    once again updated the schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,29 +1,9 @@
 drop table admin;
-drop table seller;
-drop table buyer;
-drop table publications;
-drop table people;
 drop table shopping_cart;
-
-
-create table publications (
-	id integer not null generated always as identity,
-	title varchar(40) not null,
-	price integer not null,
-	constraint chk_price check
-	(price>=0),
-	author varchar(20) not null,
-	pub_type varchar(20) not null, 
-	constraint chk_type check
-	(pub_type='book/collection' or pub_type='journal' or pub_type='conference' or pub_type='editorship'),
-	pub_year numeric(4,0),
-	isbn varchar(20),
-	picture varchar (100),
-	pause boolean not null,
-	seller_id varchar(20),
-	foreign key(seller_id) references sellers(seller_id)
-	primary key (id)
-);
+drop table publications;
+drop table sellers;
+drop table buyers;
+drop table people;
 
 create table people(
 	username varchar(32) not null,
@@ -39,35 +19,52 @@ create table people(
 );
 
 create table buyers(
-	buyer_id varchar(20) not null,
+	buyer_id varchar(32) not null,
 	credit_card bigint,
 	foreign key(buyer_id) references people(username),
 	primary key(buyer_id)
 );
 
 create table sellers(
-	seller_id varchar(20) not null,
+	seller_id varchar(32) not null,
 	paypal varchar(20) not null,
 	foreign key(seller_id) references people(username),
 	primary key(seller_id)
 );
 
-create shopping_cart(
-	cart_id varchar(20) not null,
+create table publications (
+	id integer not null generated always as identity,
+	title varchar(40) not null,
+	price integer not null,
+	constraint chk_price check
+	(price>=0),
+	author varchar(20) not null,
+	pub_type varchar(20) not null, 
+	constraint chk_type check
+	(pub_type='book/collection' or pub_type='journal' or pub_type='conference' or pub_type='editorship'),
+	pub_year numeric(4,0),
+	isbn varchar(20),
+	picture varchar (100),
+	pause boolean not null,
+	seller_id varchar(32),
+	foreign key(seller_id) references sellers(seller_id),
+	primary key (id)
+);
+
+create table shopping_cart(
+	cart_id integer not null generated always as identity,
 	added timestamp not null,
 	removed timestamp,
 	purchased timestamp,
-	publication_key varchar(20) not null,
-	buyer_key varchar(20) not null,
+	publication_key integer not null,
+	buyer_key varchar(32) not null,
 	foreign key(publication_key) references publications(id),
-	foreign key(buyer_key) references buyers(buyer_id)
+	foreign key(buyer_key) references buyers(buyer_id),
 	primary key(cart_id)
 );
 
 create table admin (
-	admin_id varchar(20) not null,
-	pub_id integer not null,
-	foreign key (admin_id) references people(username),
-	foreign key (pub_id) references publications(id),
-	primary key (admin_id, pub_id)
+	id varchar(20) not null,
+	password varchar(32) not null,
+	primary key (id)
 );
