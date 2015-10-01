26d9d8627b8890b74822d31ccdff71f8f3c964f8
davinkurniawan <davinkurniawan07@gmail.com>
2015-10-01 18:16:43 +1000

    first commit to temp

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -10,6 +10,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 	<title>Welcome!</title>
 	<link href="frontpage.css" rel="stylesheet" type="text/css">
+	<link href="resultsTable.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
--- src/edu/unsw/comp9321/web/SearchCommand.java
+++ src/edu/unsw/comp9321/web/SearchCommand.java
@@ -9,14 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import edu.unsw.comp9321.jdbc.BookStoreDAO;
+import edu.unsw.comp9321.jdbc.DAOFactory;
 import edu.unsw.comp9321.jdbc.UserDTO;
 import edu.unsw.comp9321.jdbc.UserService;
 import edu.unsw.comp9321.jdbc.Utilities;
 
 public class SearchCommand implements Command {
 	
+	private BookStoreDAO bookStoreDAO;
+	
 	public SearchCommand() {
-		
+		DAOFactory factory = DAOFactory.getInstance();
+		bookStoreDAO = factory.getBookStoreDAO();
 	}
 	
 	@Override
@@ -28,9 +33,12 @@ public class SearchCommand implements Command {
 			title = request.getParameter("title");
 		}
 		
-		UserService service = new UserService();
-		request.setAttribute("publications", service.searchPublications(title)); 
-	
-		return "/results.jsp";
+		if (request.getParameter("link") != null && request.getParameter("link").equals("true")) {
+			request.setAttribute("publication", bookStoreDAO.searchPublications(title).get(0));
+			return "/info.jsp";
+		} else {
+			request.setAttribute("publications", bookStoreDAO.searchPublications(title)); 
+			return "/results.jsp";
+		}
 	}
 }
