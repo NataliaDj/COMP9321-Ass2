46c281b9d2bf36483c3c067932db70d6b6084c5a
NataliaDj <nataliadjohari@yahoo.com.sg>
2015-10-03 16:58:36 +1000

    some errors fixed

--- src/edu/unsw/comp9321/jdbc/BookStoreDAO.java
+++ src/edu/unsw/comp9321/jdbc/BookStoreDAO.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import edu.unsw.comp9321.exception.DataAccessException;
 import edu.unsw.comp9321.exception.ServiceLocatorException;
